ad45cd338d942479b88b4a080187a7a3d7f5e151
Anone-Imouse <73369548+Anone-Imouse@users.noreply.github.com>
2020-11-28 11:36:46 -0500

    Create stop.js

---
+++ stop.js
@@ -0,0 +1,35 @@
+  var num2=0;
+
+function add(){
+  var num=document.querySelector("#num");
+    
+num2++;
+
+
+var num3=String(num2);
+
+num.innerHTML=num3;
+}
+
+var btn=document.querySelector("#btn-1");
+
+var btn2=document.querySelector("#btn-2");
+
+var btn3=document.querySelector("#btn-3");
+
+
+
+btn2.addEventListener("click",function(){
+  var count=setInterval(add,1000);
+});
+
+btn3.addEventListener("click",function(){
+  count = setInterval(add,1000);
+  num2=0;
+});
+
+btn.addEventListener("click",function(){
+clearTimeout(count)
+});
+
+count = setInterval(add,1000);
